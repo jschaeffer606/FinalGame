d0ffa201f084deb702393bb58536df3b0c80c91f
jschaef <jwschaef@ksu.edu>
2024-12-11 02:21:59 -0600

    Finished the gameplayscreen class

--- Screens/SplashScreen.cs
+++ Screens/SplashScreen.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using GameArchitectureExample.StateManagement;
+using FinalGameProject.ScreenManagement;
 
 namespace FinalGameProject.Screens
 {
